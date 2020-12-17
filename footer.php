87c30b9ad489d33ef8aad277567f47a63aff7b95
Gerardo <gerodel98@gmail.com>
2020-12-17 15:27:13 -0600

    Initial commit

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="container">
+        <!-- El parametro es igual al id del widget creado en functions.php -->
+        <?php dynamic_sidebar('footer'); ?>
+    </div>
+</footer>
+
+<?php wp_footer() ?>
+
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,75 @@
+<?php 
+
+// Add title
+function init_template(){
+
+    add_theme_support('post-thumbnails');
+    add_theme_support( 'title-tag');
+
+    register_nav_menus(
+        array(
+            'top_menu' => 'Menú Principal'
+        )
+    );
+
+}
+add_action('init', 'init_template');
+
+// DEpendencies
+function assets(){
+    wp_register_style('bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', '', '4.4.1','all');
+    wp_register_style('montserrat', 'https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap','','1.0', 'all');
+    wp_enqueue_style('estilos', get_stylesheet_uri(), array('bootstrap','montserrat'),'1.0', 'all');
+   
+    wp_register_script('popper','https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js','','1.16.0', true);
+    wp_enqueue_script('boostraps', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array('jquery','popper'),'4.4.1', true);
+    wp_enqueue_script('custom', get_template_directory_uri().'/assets/js/custom.js', '', '1.0', true);
+}
+
+add_action('wp_enqueue_scripts','assets');
+
+// Widgets
+function sidebar(){
+    register_sidebar(
+        array(
+            'name' => 'Pie de página',
+            'id'   => 'footer',
+            'description' => 'Zona de Widgets para pie de página',
+            'before_title' => '<p>',
+            'after_title'  => '</p>',
+            'before_widget' => '<div id="%1$s" class="%2$s">',
+            'after_widget'  => '</div>',
+        )
+        );
+}
+
+add_action('widgets_init', 'sidebar');
+
+// Custom types
+function productos_type(){
+    $labels = array(
+        'name' => 'Ejercicios Espirituales',
+        'singular_name' => 'EE',
+        'manu_name' => 'Ejercicios Espirituales',
+    );
+
+    $args = array(
+        'label'  => 'EE', 
+        'description' => 'Ejercicios Espirituales de Miles Christi en México',
+        'labels'       => $labels,
+        'supports'   => array('title','editor','thumbnail', 'revisions'),
+        'public'    => true,
+        'show_in_menu' => true,
+        'menu_position' => 5,
+        'menu_icon'     => 'dashicons-book',
+        'can_export' => true,
+        'publicly_queryable' => true,
+        'rewrite'       => true,
+        'show_in_rest' => true
+
+    );  
+    // Nombre de post types en singular es recomendado  
+    register_post_type('producto', $args);
+}
+
+add_action('init', 'productos_type');
